620175f43f3ac9f9e71edaf355706e5e73cc4296
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-11-15 13:40:25 -0700

    improve ye example

--- examples/cpu_usage.js
+++ examples/cpu_usage.js
@@ -1,19 +1,23 @@
 // first, we want to be able to get cpu usage stats in terms of percentage
-var cpuUsage = require('../');
-cpuUsage.on('stats', function(stats) {
+var loaded = false;
+var cpuSage = require('../');
+cpuSage.on('stats', function(stats) {
   console.log(stats);
 }).on('busy', function(percentCPU) {
+  loaded = true;
   console.log("process is overloaded, using " + percentCPU + "% cpu\n");
 }).on('normal', function(percentCPU) {
+  loaded = false;
   console.log("process is back to normal, using " + percentCPU + "% cpu\n");
 });
 
 var interval = setInterval(function() {
+  var howMuch = loaded ? 1e5 : 1e9 * 2;
   console.log("doing work");
-  for (var i = 0; i < 1e9;) i++;
+  for (var i = 0; i < howMuch;) i++;
 }, 250);
 
 process.on('SIGINT', function() {
   clearInterval(interval);
-  cpuUsage.stop();
+  cpuSage.stop();
 });
